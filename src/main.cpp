b8685efb343c0d4816dc432527743ac3d4483791
Maiddog <maiddogsrl@gmail.com>
2015-04-30 15:25:56 -0500

    Added scan_string

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,21 @@
 
 extern int yyparse();
 extern int yylex();
+extern void scan_string(const std::string& str);
 extern char *yytext;
 extern std::vector<uint8_t> *output;
 
 int main(int argc, char *argv[])
 {
-  int yydebug = 1;
-  // int token;
-  // while ((token = yylex()) != 0)
-  //   printf("Token: %d (%s)\n", token, yytext);
+
 
   puts("Parsing:");
-  std::cout << "yyparse: " << yyparse() << std::endl;
-  std::cout << "output : " << std::endl;
+
+  scan_string("LDA $0230");
+  yyparse();
+
+  // std::cout << "yyparse: " << yyparse() << std::endl;
+  // std::cout << "output : " << std::endl;
 
   for (auto byte : *output)
   {
