e5d27660c8b3b51cf0bd8bfb5eac09c0c16bb9db
Maiddog <maiddogsrl@gmail.com>
2015-05-10 18:12:54 -0500

    Add convaddr and bytesize

--- src/util.cpp
+++ src/util.cpp
@@ -14,4 +14,55 @@ namespace util
 
     return (*p == 0);
   }
+
+  enum Convertion
+  {
+    Unheadered = 0,
+    Headered,
+    PctoSnes,
+    SnestoPc = 0,
+    LoRom = 0
+  };
+
+  int convaddr(int addr, int mode = 0) 
+  {
+    bool header = mode & 1;
+    bool ispc = mode & 2;
+
+    if (ispc) 
+    {
+      if (header) 
+      {
+        addr -= 512;
+      } 
+
+      if (addr < 0 || addr >= 0x400000) 
+      {
+        return -1;
+      } 
+
+      addr = ((addr << 1) & 0x7F0000) | (addr & 0x7FFF) | 0x8000;
+
+      if ((addr & 0xF00000) == 0x700000) 
+      {
+        addr |= 0x800000;
+      } 
+    }
+    else 
+    {
+      if (addr < 0 || addr > 0xFFFFFF ||(addr & 0xFE0000) == 0x7E0000 || (addr & 0x408000) == 0x000000) 
+      {
+        return -1;
+      } 
+
+      addr = ((addr & 0x7F0000) >> 1|(addr & 0x7FFF));
+
+      if (header) 
+      {
+        addr = 512;
+      }
+    }
+
+    return addr;
+  }
 }
--- src/util.hpp
+++ src/util.hpp
@@ -9,6 +9,32 @@ namespace util
 {
   bool isnum(const std::string& s);
 
+  //  Code taken from Alcaro's post: http://smwc.me/735496
+  int convaddr(int addr, int mode);
+
+  /*
+    Returns byte size taken up by a number up to 3 bytes (max SNES limit for a parameter)
+  */
+  template <typename T> int bytesize(const T& n)
+  {
+    static_assert(std::is_integral<T>::value, "Function requires an integral type.");
+
+    if (n == (n & 0xFF))
+    {
+      return 1;
+    }
+    else if (n == (n & 0xFFFF))
+    {
+      return 2;
+    }
+    else if (n == (n & 0xFFFFFF))
+    {
+      return 3;
+    }
+
+    return 0;
+  }
+
   template <typename T> std::string to_string(const T& n)
   {
     std::ostringstream stm;
