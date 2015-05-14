00c7d66db0936d79f432ce1169dfbb34ef5a9a08
Maiddog <maiddogsrl@gmail.com>
2015-05-14 03:14:41 -0500

    Massive instruction changes
    The instruction struct now handles assembly and storing values. If an
    instruction is given a purely mathematical parameter (e.g., AND #$20 +
    2), the parameter is sent to the math parser, its size and value are
    saved, and the instruction is marked as static which means the value
    will not change.
    
    If an instruction is static, it will assemble itself into bytes to be
    extracted. If it is not static, (e.g., something like BPL label) it will
    not be assembled automatically. Instead, it must be passed a map of
    labels that will be forwarded to the math parser to get the value.
    
    As of right now, the non-static instructions are not parsed.

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -2,51 +2,144 @@
 
 namespace yasa
 {
-  Instruction::Instruction(std::string &instr, AddressMode mode, int snespos)
+  Instruction::Instruction(std::string& key, AddressMode mode, int snespos, std::string param1, std::string param2)
   {
-    m_address = snespos;
-    m_name = std::string(instr.substr(0, 3));
+    m_address.real = util::convaddr(snespos);
+    m_address.snes = snespos;
+    m_keyword = std::string(key.substr(0, 3));
     m_mode = mode;
-    m_parsed = true;
-    m_size = 1;
+    m_static = false;
+    m_setsize = false;
 
+    if (param1 != "")
+    {
+      m_params.push_back(detail::Parameter(param1));
+    }
+
+    //  Don't allow param1 to be empty if param2 is being used
+    if (param1 != "" && param2 != "")
+    {
+      m_params.push_back(detail::Parameter(param2));
+    }
+
+    //  Special case since BRK, COP, and WDM are implied but take an immediate parameter
     if (m_mode == Implied)
     {
       try
       {
-        m_opcode = get_byte(instr, mode, 1);
+        m_opcode = get_byte(key, mode, 1);
       }
       catch(InvalidInstructionException e)
       {
-        m_opcode = get_byte(instr, mode, 2);
+        m_opcode = get_byte(key, mode, 2);
       }
     }
 
-
-    if (instr.size() > 3)
+    if (key.size() > 3)
     {
       m_setsize = true;
-      switch (tolower(instr[4]))
+      switch (tolower(key[4]))
       {
         case 'b':
           m_size = 2;
           break;
         case 'w':
           m_size = 3;
+          std::cout << "Set size to 3" << std::endl;
           break;
         case 'l':
           m_size = 4;
           break;
         default:
-          throw InvalidInstructionException("Invalid size modifier: " + instr);
+          throw InvalidInstructionException("Invalid size modifier: " + key);
       }
     }
     else
     {
-      m_size = get_avg_size(instr, mode);
+      m_size = get_avg_size(key, mode);
+      std::cout << "Average size: " << m_size << std::endl;
+    }
+
+    std::map<std::string, int> temp;
+
+    if (m_params.size() == 0)
+    {
+      m_static = true;
+    }
+    else if (m_params.size() == 1)
+    {
+      int result = math_parse(m_params[0].string);
+
+      if (result <= INT24_MAX)
+      {
+        m_static = true;
+
+        if (m_setsize == false)
+        {
+          int result = math_result();
+          m_params[0].value = result;
+          m_params[0].size = math_get_size();
+          m_size = math_get_size() + 1;
+          std::cout << "One param size: " << math_get_size() << std::endl;
+        }
+      }
+    }
+    else if (m_params.size() == 2)
+    {
+      int result = math_parse(m_params[0].string);
+      
+      if (result <= INT24_MAX)
+      {
+        m_params[0].value = result;
+        m_params[0].size = math_get_size();
+
+        result = math_parse(m_params[1].string);
+        
+        if (result <= INT24_MAX)
+        {
+          m_static = true;
+
+          m_params[1].value = result;
+          m_params[1].size = math_get_size();
+
+          if (m_setsize == false)
+          {
+            m_size = m_params[0].size + m_params[1].size + 1;
+          }
+        }
+      }
+    }
+
+    std::cout << key << " + " << param1 << " + " << param2 << " " << m_size << std::endl;
+
+    if (m_static)
+    {
+      this->compile();
     }
   }
 
+  void Instruction::compile()
+  {
+    m_opcode = get_byte(m_keyword, m_mode, m_size);
+
+    m_data.clear();
+    m_data.push_back(m_opcode);
+
+    for (auto param : m_params)
+    {
+      for (int i = 0; i < param.size; i++)
+      {
+        m_data.push_back((param.value) & (0xFF << (i * 8))); 
+      }
+    }
+
+    while (m_data.size() < m_size)
+    {
+      m_data.push_back(0);
+    }
+  }
+
+  /*
   Instruction& Instruction::add(std::string str)
   {
     m_parsed = false;
@@ -128,4 +221,5 @@ namespace yasa
   {
     return m_mode;
   }
+  */
 }
--- src/instruction.hpp
+++ src/instruction.hpp
@@ -10,8 +10,33 @@
 #include "assembler.hpp"
 #include "externs.hpp"
 
+#define INT24_MAX 0xFFFFFF
+
 namespace yasa
 {
+  namespace detail
+  {
+    struct Parameter
+    {
+      Parameter(std::string str, int value = 0, int size = 0)
+      {
+        string = str;
+        value = value;
+        size = size;
+      }
+
+      std::string string;
+      int value;
+      int size;
+    };
+  }
+
+  struct Address
+  {
+    int real;
+    int snes;
+  };
+
   class InvalidInstructionException : public std::runtime_error
   {
   public:
@@ -20,33 +45,66 @@ namespace yasa
 
   struct Instruction
   {
-    Instruction(std::string &instr, AddressMode mode, int snespos);
+    Instruction(std::string& key, AddressMode mode, int snespos, std::string param1 = "", std::string param2 = "");
 
-    Instruction& add(Integer *n);
-    Instruction& add(int n, int size);
-    Instruction& add(std::string str);
-    Instruction& defer(std::string& expression);
+    inline std::string key()
+    {
+      return m_keyword;
+    }
 
-    bool parsed();
+    inline std::string type()
+    {
+      return AddressModeString[m_mode];
+    }
 
-    std::vector<uint8_t> data();
-    int size();
-    uint8_t opcode();
-    int address();
-    std::string name();
-    AddressMode mode();
+    inline int size()
+    {
+      return m_size;
+    }
+
+    inline int pc_addr()
+    {
+      return m_address.real;
+    }
+
+    inline int snes_addr()
+    {
+      return m_address.snes;
+    }
+
+    inline std::string get_param(int param)
+    {
+      if (m_params.size() > param)
+      {
+        return m_params[param - 1].string;
+      }
+
+      return "";
+    }
+
+    inline bool is_static()
+    {
+      return m_static;
+    }
+
+    inline std::vector<uint8_t> data()
+    {
+      return m_data;
+    }
+
   private:
-    std::vector<std::string> m_params;
+    std::string m_keyword;
+    AddressMode m_mode;
+    Address m_address;
+    int m_size;
+    std::vector<detail::Parameter> m_params;
     std::vector<uint8_t> m_data;
 
-    int m_address;
-    bool m_parsed;
     bool m_setsize;
-
-    std::string m_name;
-    AddressMode m_mode;
-    int m_size;
+    bool m_static;  //  Param has no labels and doesn't need to be reparsed
     uint8_t m_opcode;
+
+    void compile();
   };
 }
 
